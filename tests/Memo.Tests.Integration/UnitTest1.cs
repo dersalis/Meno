95543dbd2f7cf68aa80bd4821f7544d36c353282
Damian <damian.ruta@hotmail.com>
2024-04-18 12:32:26 +0200

    Test projects

---
+++ tests/Memo.Tests.Integration/UnitTest1.cs
@@ -0,0 +1,10 @@
+namespace Memo.Tests.Integration;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
---
+++ tests/Memo.Tests.Unit/UnitTest1.cs
@@ -0,0 +1,10 @@
+namespace Memo.Tests.Unit;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
